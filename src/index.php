dba0789575e08a2081641780c2518f5e2678c371
Moustapha Boulgoudan <moustapha@devops.local>
2024-04-14 21:35:35 -0400

    Add the Manager to the list of the departments

--- src/index.php
+++ src/index.php
@@ -26,8 +26,12 @@
 
     $con = new PDO("mysql:host=$MYSQL_HOST;dbname=$MYSQL_DATABASE", $MYSQL_USER , $MYSQL_PASSWORD);
     $query = "
-    SELECT dept_no, dept_name
-    FROM   departments
+    SELECT     dept.dept_no, dept.dept_name, CONCAT_WS(' ', emp.first_name,emp.last_name) AS 'Manager', dm.from_date as 'From Date' 
+    FROM       departments dept 
+    INNER JOIN dept_manager dm ON dm.dept_no = dept.dept_no 
+    INNER JOIN employees emp ON emp.emp_no = dm.emp_no 
+    WHERE      dm.to_date = '9999-01-01' 
+    ORDER BY   dept.dept_no;
     ";
     $stmt = $con->prepare($query);
     $stmt->execute();
@@ -37,12 +41,12 @@
       ?>
       <h2>Departments</h2>
       <table>
-        <th>Dept. No</th><th>Dept. Name</th>
+        <th>Dept. No</th><th>Dept. Name</th><th>Dept. Manager</th>
       <?php
       foreach ($stmt as $row) {
         ?>
         <tr>
-          <td><?=$row[0]?></td><td><?=$row[1]?></td>
+          <td><?=$row[0]?></td><td><?=$row[1]?></td><td><?=$row[2]?><BR><i>since <?=$row[3]?></i></td>
         </tr>
         <?php
       }
